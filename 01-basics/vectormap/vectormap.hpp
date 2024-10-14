0a7ff2432ae372c87d8c4124cfbcc5ea2c85bb08
Gromiusz <kgorka22@gmail.com>
2024-10-14 09:53:47 +0200

    add specialization template

--- 01-basics/vectormap/vectormap.hpp
+++ 01-basics/vectormap/vectormap.hpp
@@ -20,6 +20,24 @@ public:
     bool isIntKey();
 };
 
+template <typename ValueType>
+class VectorMap<bool, ValueType>
+{
+    static_assert(std::is_constructible<ValueType>::value, "ValueType have to have default constructor");
+    ValueType true_value;
+    ValueType false_value;
+    bool false_value_initialized = false;
+    bool true_value_initialized = false;
+
+public:
+    static constexpr bool is_int_key = false;
+
+    void insert(bool key, ValueType value);
+    ValueType &operator[](bool key);
+    ValueType &at(bool key);
+    bool isIntKey();
+};
+
 template <typename KeyType, typename ValueType>
 void VectorMap<KeyType, ValueType>::insert(KeyType key, ValueType value)
 {
@@ -56,4 +74,59 @@ template <typename KeyType, typename ValueType>
 bool VectorMap<KeyType, ValueType>::isIntKey()
 {
     return std::is_integral<KeyType>::value;
+}
+
+/////////////////////////////////////////
+
+template <typename ValueType>
+void VectorMap<bool, ValueType>::insert(bool key, ValueType value)
+{
+    if(key)
+    {
+        true_value = value;
+        true_value_initialized = true;
+    }
+    else
+    {
+        false_value = value;
+        false_value_initialized = true;
+    }
+}
+
+template <typename ValueType>
+ValueType &VectorMap<bool, ValueType>::operator[](bool key)
+{
+    if(key)
+    {
+        if(true_value_initialized) { return true_value; }
+        true_value = ValueType{};
+        return true_value;
+    }
+    else
+    {
+        if(false_value_initialized) { return false_value; }
+        false_value = ValueType{};
+        return false_value;
+    }
+}
+
+template <typename ValueType>
+ValueType &VectorMap<bool, ValueType>::at(bool key)
+{
+    if(key)
+    {
+        if(true_value_initialized) return true_value;
+        throw std::out_of_range("Key has not been initializated");
+    }
+    else
+    {
+        if(false_value_initialized) return false_value;
+        throw std::out_of_range("Key has not been initializated");
+    }
+}
+
+template<typename ValueType>
+bool VectorMap<bool, ValueType>::isIntKey()
+{
+    return false;
 }
